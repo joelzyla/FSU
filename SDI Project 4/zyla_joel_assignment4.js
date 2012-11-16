4c903b774c46e2f2019f91d8328646957902312a
Joel Zyla <joelzyla@Joels-MacBook-Pro.local>
2012-11-15 20:08:12 -0500

    Finished with function 5 - regex URL match.

--- SDI Project 4/zyla_joel_assignment4.js
+++ SDI Project 4/zyla_joel_assignment4.js
@@ -7,12 +7,13 @@ alert("JavaScript works!");
 var myNumber = 15.5;
 var myString = "25";
 var myArray = ["sit", 4, "speak", 6, 9];
-var myRegexObj = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+var myRegexPhone = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+var myRegexURL = /(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?/;
 var myPhoneNumber = "304-254-1212"
+var myURL = "http://www.pilotincontrol.com"
 
 //LIBRARY
 var myLibrary = function() {
-	
 	//FUNCTION 1 - Format a number to use a specific number of decimal places, as for money: 2.1 → 2.10.
 	var formatMoney = function(number) {
 		//remove special characters from number
@@ -54,15 +55,32 @@ var myLibrary = function() {
 
 	//FUNCTION 4 - Does a string follow a 123-456-7890 pattern like a phone number?
 	var ifPhoneNumber = function(myNumber) {
-			if(myRegexObj.test(myNumber)) {
+		if(myRegexPhone.test(myNumber)) {
 				return true;
-			} else {
+		} else {
 				return false;
-			}
-	}
-			
+		}
+	}		
 			
 	//FUNCTION 5 - Is the string a URL? (Does it start with http: or https:?)
+	var checkURL = function(URL) {
+		if(myRegexURL.test(URL)) {
+			return "is";
+		} else {
+			return "isn't";
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	//Return object to capture what is being returned by the internal function.
 	//This function will return a value to myLibrary which then returns it to the code when called upon.
@@ -71,7 +89,8 @@ var myLibrary = function() {
 		"formatMoney": formatMoney,
 		"stringToNumber": stringToNumber,
 		"totalValue": totalValue,
-		"ifPhoneNumber": ifPhoneNumber
+		"ifPhoneNumber": ifPhoneNumber,
+		"checkURL": checkURL
 	}
 	
 //END OF LIBRARY
@@ -87,3 +106,8 @@ console.log("The string converts to number " + myLibrary.stringToNumber(myString
 console.log("The total value of the numbers in the array is " + myLibrary.totalValue(myArray) + ".");
 
 console.log("It is " + myLibrary.ifPhoneNumber(myPhoneNumber) + " that " + myPhoneNumber + " is in the correct phone number format (XXX-XXX-XXXX).");
+
+console.log("The string " + myLibrary.checkURL(myURL) + " a URL.");
+
+
+
