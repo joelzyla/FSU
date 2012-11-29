0c74c95bc60671ace52ab023ea4e8e984215f9c2
Joel Zyla <joelzyla@Joels-MacBook-Pro.local>
2012-11-29 16:42:25 -0500

    Initial upload with stub programming and comments added

---
+++ Visual Frameworks/zyla_joel_project2/js/main.js
@@ -0,0 +1,26 @@
+
+
+// ***SAVE DATA FUNCTION***
+// Write a saveData function that saves all data input into the form into Local Storage.
+// This means the values for your input, select, radio or checkboxes etc.
+// You should also save a key(random number) for your values(array or object). Each saved entry will be a key:value pair.
+// The key should be a random number which will create a unique id.
+// The value should be an array or object that contains all the values of your form field data.
+// This allows for our form to save multiple submissions into Local Storage without overwriting data.
+// Remember you can test your local storage by using the Web Inspector
+
+// ***GET DATA***
+// Once data has been submitted through the form, we can grab the data submitted from Local Storage. 
+// List out your saved data from Local Storage on the additem.html document using the Display Data link you created. 
+// Make sure you hide the form when you display your data on screen as shown in the example screenshots. 
+// You can use these the following screenshots as a guideline. 
+// Your project's screen will vary from this example based on your topic.
+
+// ***CLEAR DATA***
+// Create a function that removes all data from local storage.
+// Be sure to create a clear data link or button in your app that runs the function that removes all data.
+
+// ***ARRAY FUNCTION***
+// Write an array function(s) that populates at least one select form element. 
+// This probably will be for your categories but you may have more select fields for other things depending on the topic you chose. 
+// This will REPLACE your static HTML select element(s) so that you can see an alterative way to create HTML elements.
