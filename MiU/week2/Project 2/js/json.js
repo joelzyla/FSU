cbe0f92fd64d15b8964b6e0300e853aba9f4f161
Joel Zyla <joelzyla@Joels-MacBook-Pro.local>
2013-01-13 08:46:23 -0500

    Initial meaningful commit. Added all files from project 1 to new project 2 folder.

---
+++ MiU/week2/Project 2/js/json.js
@@ -0,0 +1,225 @@
+
+//Project 4: myOptions
+//By Joel Zyla
+//FSU Visual Frameworks 1212
+
+var json = {
+	//object should be consistant with form. 
+	//use the same properties as the storedata object
+	//so data in local storage is consistant with json data
+	"position1": { //object name
+		//first property group : value of group
+		"callput": ["Contract type:", "Call"], //property value pair
+		"ssymbol": ["Stock Symbol:", "GOOG"],
+		"sprice": ["Strike Price:", "600"],
+		"allornone": ["All or none?", "yes"],
+		"edate": ["Date:", "2013-06-20"],
+		"notes": ["Notes:", "Sell at 610."]
+	},
+	"position2": {
+		"callput": ["Contract type:", "Put"],
+		"ssymbol": ["Stock Symbol:", "DDD"],
+		"sprice": ["Strike Price:", "28"],
+		"allornone": ["All or none?", "yes"],
+		"edate": ["Date:", "2013-01-20"],
+		"notes": ["Notes:", "Sell at 25."]
+	},
+	"position3": { 
+		"callput": ["Contract type:", "Call"],
+		"ssymbol": ["Stock Symbol:", "GRPN"],
+		"sprice": ["Strike Price:", "5"],
+		"allornone": ["All or none?", "yes"],
+		"edate": ["Date:", "2013-03-20"],
+		"notes": ["Notes:", "Sell at 6."]
+	},
+	"position4": { 
+		"callput": ["Contract type:", "Stock"],
+		"ssymbol": ["Stock Symbol:", "GOOG"],
+		"sprice": ["Strike Price:", "5"],
+		"allornone": ["All or none?", "yes"],
+		"edate": ["Date:", "2013-03-20"],
+		"notes": ["Notes:", "Sell at 6."]
+	},
+	"position5": { //object name
+		//first property group : value of group
+		"callput": ["Contract type:", "Call"], //property value pair
+		"ssymbol": ["Stock Symbol:", "AAPL"],
+		"sprice": ["Strike Price:", "600"],
+		"allornone": ["All or none?", "yes"],
+		"edate": ["Date:", "2013-06-20"],
+		"notes": ["Notes:", "Sell at 610."]
+	},
+	"position6": {
+		"callput": ["Contract type:", "Put"],
+		"ssymbol": ["Stock Symbol:", "DDD"],
+		"sprice": ["Strike Price:", "28"],
+		"allornone": ["All or none?", "yes"],
+		"edate": ["Date:", "2013-01-20"],
+		"notes": ["Notes:", "Sell at 25."]
+	},
+	"position7": { 
+		"callput": ["Contract type:", "Call"],
+		"ssymbol": ["Stock Symbol:", "GOOG"],
+		"sprice": ["Strike Price:", "5"],
+		"allornone": ["All or none?", "yes"],
+		"edate": ["Date:", "2013-03-20"],
+		"notes": ["Notes:", "Sell at 6."]
+	},
+	"position8": { 
+		"callput": ["Contract type:", "Stock"],
+		"ssymbol": ["Stock Symbol:", "GRPN"],
+		"sprice": ["Strike Price:", "5"],
+		"allornone": ["All or none?", "yes"],
+		"edate": ["Date:", "2013-03-20"],
+		"notes": ["Notes:", "Sell at 6."]
+	},
+	"position9": { 
+		"callput": ["Contract type:", "Stock"],
+		"ssymbol": ["Stock Symbol:", "ZNGA"],
+		"sprice": ["Strike Price:", "5"],
+		"allornone": ["All or none?", "yes"],
+		"edate": ["Date:", "2013-03-20"],
+		"notes": ["Notes:", "Sell at 6."]
+	},
+	"position10": { //object name
+		//first property group : value of group
+		"callput": ["Contract type:", "Call"], //property value pair
+		"ssymbol": ["Stock Symbol:", "AAPL"],
+		"sprice": ["Strike Price:", "600"],
+		"allornone": ["All or none?", "yes"],
+		"edate": ["Date:", "2013-06-20"],
+		"notes": ["Notes:", "Sell at 610."]
+	},
+	"position11": {
+		"callput": ["Contract type:", "Put"],
+		"ssymbol": ["Stock Symbol:", "DDD"],
+		"sprice": ["Strike Price:", "28"],
+		"allornone": ["All or none?", "yes"],
+		"edate": ["Date:", "2013-01-20"],
+		"notes": ["Notes:", "Sell at 25."]
+	},
+	"position12": { 
+		"callput": ["Contract type:", "Call"],
+		"ssymbol": ["Stock Symbol:", "GOOG"],
+		"sprice": ["Strike Price:", "5"],
+		"allornone": ["All or none?", "yes"],
+		"edate": ["Date:", "2013-03-20"],
+		"notes": ["Notes:", "Sell at 6."]
+	},
+	"position13": { 
+		"callput": ["Contract type:", "Stock"],
+		"ssymbol": ["Stock Symbol:", "ZNGA"],
+		"sprice": ["Strike Price:", "5"],
+		"allornone": ["All or none?", "yes"],
+		"edate": ["Date:", "2013-03-20"],
+		"notes": ["Notes:", "Sell at 6."]
+	},
+	"position14": { //object name
+		//first property group : value of group
+		"callput": ["Contract type:", "Call"], //property value pair
+		"ssymbol": ["Stock Symbol:", "AAPL"],
+		"sprice": ["Strike Price:", "600"],
+		"allornone": ["All or none?", "yes"],
+		"edate": ["Date:", "2013-06-20"],
+		"notes": ["Notes:", "Sell at 610."]
+	},
+	"position15": {
+		"callput": ["Contract type:", "Put"],
+		"ssymbol": ["Stock Symbol:", "DDD"],
+		"sprice": ["Strike Price:", "28"],
+		"allornone": ["All or none?", "yes"],
+		"edate": ["Date:", "2013-01-20"],
+		"notes": ["Notes:", "Sell at 25."]
+	},
+	"position16": { 
+		"callput": ["Contract type:", "Call"],
+		"ssymbol": ["Stock Symbol:", "ZNGA"],
+		"sprice": ["Strike Price:", "5"],
+		"allornone": ["All or none?", "yes"],
+		"edate": ["Date:", "2013-03-20"],
+		"notes": ["Notes:", "Sell at 6."]
+	},
+	"position17": { 
+		"callput": ["Contract type:", "Stock"],
+		"ssymbol": ["Stock Symbol:", "GRPN"],
+		"sprice": ["Strike Price:", "5"],
+		"allornone": ["All or none?", "yes"],
+		"edate": ["Date:", "2013-03-20"],
+		"notes": ["Notes:", "Sell at 6."]
+	},
+	"position18": { //object name
+		//first property group : value of group
+		"callput": ["Contract type:", "Call"], //property value pair
+		"ssymbol": ["Stock Symbol:", "GOOG"],
+		"sprice": ["Strike Price:", "600"],
+		"allornone": ["All or none?", "yes"],
+		"edate": ["Date:", "2013-06-20"],
+		"notes": ["Notes:", "Sell at 610."]
+	},
+	"position19": {
+		"callput": ["Contract type:", "Put"],
+		"ssymbol": ["Stock Symbol:", "DDD"],
+		"sprice": ["Strike Price:", "28"],
+		"allornone": ["All or none?", "yes"],
+		"edate": ["Date:", "2013-01-20"],
+		"notes": ["Notes:", "Sell at 25."]
+	},
+	"position20": { 
+		"callput": ["Contract type:", "Call"],
+		"ssymbol": ["Stock Symbol:", "ZNGA"],
+		"sprice": ["Strike Price:", "5"],
+		"allornone": ["All or none?", "yes"],
+		"edate": ["Date:", "2013-03-20"],
+		"notes": ["Notes:", "Sell at 6."]
+	},
+	"position21": { 
+		"callput": ["Contract type:", "Stock"],
+		"ssymbol": ["Stock Symbol:", "GRPN"],
+		"sprice": ["Strike Price:", "5"],
+		"allornone": ["All or none?", "yes"],
+		"edate": ["Date:", "2013-03-20"],
+		"notes": ["Notes:", "Sell at 6."]
+	},
+	"position22": { //object name
+		//first property group : value of group
+		"callput": ["Contract type:", "Call"], //property value pair
+		"ssymbol": ["Stock Symbol:", "GOOG"],
+		"sprice": ["Strike Price:", "600"],
+		"allornone": ["All or none?", "yes"],
+		"edate": ["Date:", "2013-06-20"],
+		"notes": ["Notes:", "Sell at 610."]
+	},
+	"position23": {
+		"callput": ["Contract type:", "Put"],
+		"ssymbol": ["Stock Symbol:", "DDD"],
+		"sprice": ["Strike Price:", "28"],
+		"allornone": ["All or none?", "yes"],
+		"edate": ["Date:", "2013-01-20"],
+		"notes": ["Notes:", "Sell at 25."]
+	},
+	"position24": {
+		"callput": ["Contract type:", "Put"],
+		"ssymbol": ["Stock Symbol:", "DDD"],
+		"sprice": ["Strike Price:", "28"],
+		"allornone": ["All or none?", "yes"],
+		"edate": ["Date:", "2013-01-20"],
+		"notes": ["Notes:", "Sell at 25."]
+	},
+	"position25": {
+		"callput": ["Contract type:", "Put"],
+		"ssymbol": ["Stock Symbol:", "ZNGA"],
+		"sprice": ["Strike Price:", "28"],
+		"allornone": ["All or none?", "yes"],
+		"edate": ["Date:", "2013-01-20"],
+		"notes": ["Notes:", "Sell at 25."]
+	},
+	"position26": { 
+		"callput": ["Contract type:", "Call"],
+		"ssymbol": ["Stock Symbol:", "GOOG"],
+		"sprice": ["Strike Price:", "5"],
+		"allornone": ["All or none?", "yes"],
+		"edate": ["Date:", "2013-03-20"],
+		"notes": ["Notes:", "Sell at 6."]
+	}
+
+}
