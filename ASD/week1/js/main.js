b719fdc33d0da27223baeb86ffa528871890e322
Joel Zyla <joelzyla@gmail.com>
2013-03-07 23:20:30 -0500

    Uploaded modified apps

---
+++ ASD/week1/js/main.js
@@ -0,0 +1,9 @@
+//By Joel Zyla
+
+
+$('#home').on('pageinit', function(){
+	//code needed for home page goes here
+});	
+		
+
+//The functions below can go inside or outside the pageinit function for the page in which it is needed.
